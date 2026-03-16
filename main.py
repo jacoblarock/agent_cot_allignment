c6539bb5cd453e24f8cbda3c1e1cd6b4e96863fc
Jacob LaRock <jacoblarock@gmail.com>
2026-03-16 10:27:44 +0100

    add main function

---
+++ main.py
@@ -0,0 +1,31 @@
+import models
+import json
+
+def main():
+    train_split = 0.8
+    with open("data/prompts.json") as file:
+        prompts = json.load(file)
+    with open("data/aligned.json") as file:
+        aligned = json.load(file)
+    with open("data/misaligned.json") as file:
+        misaligned = json.load(file)
+    split_index = int(len(prompts) * train_split)
+    print(split_index)
+    p_train = prompts[:split_index]
+    p_test = prompts[split_index:]
+    a_train = aligned[:split_index]
+    a_test = aligned[split_index:]
+    m_test = misaligned[split_index:]
+    print(len(a_train))
+    print(len(a_test))
+    embedder = models.create_model()
+    evaluator = models.create_model()
+    y = models.predict(embedder, p_train)
+    evaluator = models.fit(evaluator, a_train, y, epochs=100)
+    test_aligned = models.eval(embedder, evaluator, p_test, a_test)
+    print(test_aligned)
+    test_misaligned = models.eval(embedder, evaluator, p_test, m_test)
+    print(test_misaligned)
+
+if __name__ == "__main__":
+    main()
